ff66764a6a3d4f69b6b4ae7fb3ce374d6ef2dcce
Allan <altonallan47@gmail.com>
2026-02-14 12:42:54 +0300

    Update Sidebar.tsx

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 h-screen border-r border-[var(--border-light)] p-6 bg-white">
+    <aside className="w-64 h-screen border-r `border-(--border-light)` p-6 bg-white">
       <h2 className="text-lg font-semibold mb-8">Inventory</h2>
 
       <nav className="flex flex-col gap-4 text-sm">
